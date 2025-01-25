6acc69f1cac09218f87e1ce4776190d29e673040
Yago <yagodominguezm@gmail.com>
2025-01-25 10:31:07 +0100

    Creación del proyecto y primeras lineas
    Se crea el proyecto de blackjack y se empieza con alguna línea de código

---
+++ Blackjack/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+
+class Entrada
+{
+
+
+public static void Main(String[] args)
+{
+
+ArrayList baraja = new ArrayList();
+
+string[] palos = {"Picas","Treboles","Corazones","Diamantes"};
+string[] numeros = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+
+
+
+
+
+}
+
+
+
+
+
+
+}
