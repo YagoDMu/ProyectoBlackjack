e88ee08d98c503de3c51ab29494e9c651a9b700a
David Duirs <duirsdevelopment@gmail.com>
2025-01-31 13:08:20 +0100

    Update Baraja.cs

--- Blackjack/Baraja.cs
+++ Blackjack/Baraja.cs
@@ -5,15 +5,13 @@ using System.Runtime.CompilerServices;
 class Baraja
 {
     private string[] palos = {"\u2660","\u2663","\u2665","\u2666"};
-    private string[] numeros = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+    private string[] numeros = {" A"," 2"," 3"," 4"," 5"," 6"," 7"," 8"," 9","10"," J"," Q"," K"};
 
     private Dictionary<string,int> baraja = new Dictionary<string,int>();
     
-
-
     public void recogeBaraja()
     {
-        // baraja.Clear();
+        baraja.Clear();
         string key;
         int j;
         for (int i = 0; i < numeros.Length; i++)
@@ -27,31 +25,30 @@ class Baraja
                 {
                     j=10;
                 }
-                key = numeros[i]+palos[p];
+                key = numeros[i]+" "+palos[p];
                 baraja.Add(key,j);
             }
-
             //Console.WriteLine(baraja.Keys);
         }  
     }
 
     public void mostrarBaraja()
-
     {
         foreach(var carta in baraja)
         {
-            Console.WriteLine(carta.Key + " Valor: "+ carta.Value);
-            Console.WriteLine();
+            Console.WriteLine(" Carta Key: "+carta.Key + " Valor: "+ carta.Value);
+            // Console.WriteLine();
         }
     }
 
     public KeyValuePair<string,int> robarCarta()
     {
         Random random = new Random();
-
-        var cartaRobada = baraja.ElementAt(random.Next(0,52));
+        var cartaRobada = baraja.ElementAt(random.Next(0,baraja.Count()));
         baraja.Remove(cartaRobada.Key);
-
+            // catch (System.ArgumentOutOfRangeException){
+            //     Console.WriteLine(" \n #### Excepcion: ArgumentOutOfRangeException ### \n");
+            // }
         return cartaRobada;
         
         // Console.WriteLine(baraja.ElementAt(random.Next(0,53)).Key);
@@ -59,7 +56,4 @@ class Baraja
     }
 
     
-
-
-
 }
